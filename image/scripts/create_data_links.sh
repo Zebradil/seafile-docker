81faee3ff2df9ac2b8fb9c835719395d72c77ede
German Lashevich <german.lashevich@gmail.com>
2019-06-05 00:27:48 +0200

    Intermediate state

---
+++ image/scripts/create_data_links.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [[ $SEAFILE_BOOTSRAP != "" ]]; then
+    exit 0
+fi
+
+if [[ $TIME_ZONE != "" ]]; then
+    time_zone=/usr/share/zoneinfo/$TIME_ZONE
+    if [[ ! -e $time_zone ]]; then
+        echo "invalid time zone"
+        exit 1
+    else
+        ln -snf $time_zone /etc/localtime
+        echo "$TIME_ZONE" > /etc/timezone
+    fi
+fi
+
+dirs=(
+    conf
+    ccnet
+    seafile-data
+    seahub-data
+    pro-data
+    seafile-license.txt
+)
+
+for d in ${dirs[*]}; do
+    src=/shared/seafile/$d
+    if [[ -e $src ]]; then
+        rm -rf /opt/seafile/$d && ln -sf $src /opt/seafile
+    fi
+done
+
+if [[ ! -e /shared/logs/seafile ]]; then
+    mkdir -p /shared/logs/seafile
+fi
+rm -rf /opt/seafile/logs && ln -sf /shared/logs/seafile/ /opt/seafile/logs
